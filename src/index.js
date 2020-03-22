93b3cb687aa8c8e3cea031702bb94892aabab04d
Richard Antao <richardmantao@gmail.com>
2020-03-21 22:12:17 -0400

    Imported bootstrap css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
+import { Router } from "react-router-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+
+render(
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.querySelector("#root")
 );
 
